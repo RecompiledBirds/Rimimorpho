8fdb5812386974dc18503f4ffdd55087f4f174bd
RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2023-11-21 14:29:29 -0500

    Store races an amphimorpho knows

---
+++ Source/RaceStorage/StoredRace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+namespace Rimimorpho
+{
+    public class StoredRace : IExposable, ILoadReferenceable
+    {
+        public ThingDef storedDef;
+
+        public virtual void ExposeData()
+        {
+            Scribe_Defs.Look(ref storedDef, nameof(storedDef));
+
+        }
+
+        public string GetUniqueLoadID()
+        {
+           return $"{this.GetHashCode()}";
+        }
+    }
+
+    public class StoredRaceWithXenoType : StoredRace
+    {
+        public XenotypeDef storedXenotypeDef;
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref storedXenotypeDef, nameof(storedXenotypeDef));
+        }
+    }
+}

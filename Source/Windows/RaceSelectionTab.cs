99fbafd7e8f823b2a00f03ff5dce615eec495b25
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2024-01-09 16:58:33 -0500

    cleanup

--- Source/Windows/RaceSelectionTab.cs
+++ Source/Windows/RaceSelectionTab.cs
@@ -20,6 +20,7 @@ namespace Rimimorpho
             Widgets.BeginScrollView(inRect, ref scrollPos, viewRect);
            
             float xPos = inRect.position.x + 60;
+            float textureX = inRect.position.x + 10;
             List<ThingDef> species = shifter.knownSpecies.Keys.ToList();
             int length = 1;
             for (int i = 0; i < shifter.knownSpecies.Count; i++)
@@ -27,23 +28,23 @@ namespace Rimimorpho
                 List<StoredRace> storedRaces =shifter.knownSpecies[species[i]];
                 if (storedRaces.NullOrEmpty()) continue;
                 Texture2D tex = species[i].uiIcon;
-                Widgets.DrawTextureFitted(new Rect(new Vector2(inRect.position.x + 10,boxHeight*length+20),new Vector2(boxHeight,boxHeight)),tex,1);
+                Widgets.DrawTextureFitted(new Rect(new Vector2(textureX,boxHeight*length+20),new Vector2(boxHeight,boxHeight)),tex,1);
                 for(int a =0; a < storedRaces.Count; a++)
                 {
                     StoredRace race = storedRaces[a];
                     if (a == 0)
                     {
-                        Widgets.Label(new Rect(new Vector2(inRect.position.x + 90, boxHeight * length + 20), new Vector2(inRect.width - 90, boxHeight)), (race.XenotypeDef != null ? race.XenotypeDef.defName : race.ThingDef.defName));
+                        Widgets.Label(new Rect(new Vector2(xPos, boxHeight * length + 20), new Vector2(inRect.width - 90, boxHeight)), (race.XenotypeDef != null ? race.XenotypeDef.defName : race.ThingDef.defName));
                         continue;
                     }
                     if (a > 0)
                     {
                         Texture2D icon = tex;
                         if (race.XenotypeDef != null&&race.XenotypeDef.Icon!=null) icon = race.XenotypeDef.Icon;
-                        Widgets.DrawTextureFitted(new Rect(new Vector2(inRect.position.x + 10, boxHeight * length + 20), new Vector2(boxHeight, boxHeight)), icon, 1);
+                        Widgets.DrawTextureFitted(new Rect(new Vector2(textureX, boxHeight * length + 20), new Vector2(boxHeight, boxHeight)), icon, 1);
                        
                     }
-                    Widgets.Label(new Rect(new Vector2(inRect.position.x + 90, boxHeight * length + 20 + boxHeight), new Vector2(inRect.width - 90 - boxHeight, boxHeight)), (race.XenotypeDef != null ? race.XenotypeDef.defName : race.ThingDef.defName));
+                    Widgets.Label(new Rect(new Vector2(xPos, boxHeight * length + 20 + boxHeight), new Vector2(inRect.width - 90 - boxHeight, boxHeight)), (race.XenotypeDef != null ? race.XenotypeDef.defName : race.ThingDef.defName));
                     length++;
                 }
                 length++;

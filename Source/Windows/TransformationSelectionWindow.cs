0a46e7d1f95d014a129d18168a8728120aa22b43
Dani <daniel_plaster@live.de>
2024-01-10 18:40:26 +0100

    windowprogress

--- Source/Windows/TransformationSelectionWindow.cs
+++ Source/Windows/TransformationSelectionWindow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HarmonyLib;
+using RVCRestructured.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,8 @@ namespace Rimimorpho
 {
     internal class TransformationSelectionWindow : Window
     {
+        private const float bttnHeight = 45f;
+        private const float bttnDescHeight = 16f;
         private const float margin = 5f;
 
         private static readonly Vector2 initSize = new Vector2(300, 600);
@@ -20,19 +24,36 @@ namespace Rimimorpho
         private static readonly Rect titleRect = new Rect(topBarRect.x, topBarRect.y, topBarRect.width - topBarRect.height - 4f, topBarRect.height);
         private static readonly Rect closBttnRect = new Rect(topBarRect.xMax - topBarRect.height, topBarRect.y, topBarRect.height, topBarRect.height + 1f);
 
-        public new bool draggable = true;
-        public new bool onlyOneOfTypeAllowed = true;
 
         private readonly Pawn pawn;
+        private readonly AmphiShifter pawnAmphiShifter;
+        private readonly List<StoredRace> storedPawnRaces;
+
         private readonly Color originalColor;
         private readonly GameFont originalFont;
+        
+        private Rect scrollAreaRect = new Rect(windowMargin.x, windowMargin.y + topBarRect.height + margin, windowMargin.width, windowMargin.height - margin - topBarRect.height);
+        private Rect scrollAreaInnerRect = new Rect();
+        private Vector2 scrollHeight = new Vector2();
+
+        private int selectedRace = -1;
+
+        private Vector2 BaseSpeciesBttnSize => new Vector2(scrollAreaInnerRect.size.x, bttnHeight);
+
 
         public TransformationSelectionWindow(Pawn pawn) 
         {
             this.pawn = pawn;
             
+            onlyOneOfTypeAllowed = true;
+
             originalFont = Text.Font;
             originalColor = GUI.color;
+
+            pawnAmphiShifter = pawn.TryGetComp<AmphiShifter>();
+            storedPawnRaces = pawnAmphiShifter?.knownRaces;
+            scrollAreaInnerRect = scrollAreaRect.GetInnerScrollRect((bttnHeight + margin) * storedPawnRaces.Count);
+            Log.Message(storedPawnRaces.Join((x) => x.ToString()));
         }
 
         public override Vector2 InitialSize => initSize;
@@ -46,10 +67,31 @@ namespace Rimimorpho
         }
 
         //TODO: Translations strings
-        public override void DoWindowContents(Rect inRect)
+        public override void DoWindowContents(Rect _)
         {
             Text.Font = GameFont.Medium;
             Widgets.Label(titleRect, $"Select Transformation:");
+            Widgets.BeginScrollView(scrollAreaRect, ref scrollHeight, scrollAreaInnerRect);
+            Widgets.BeginGroup(scrollAreaRect);
+            for (int i = 0; i < storedPawnRaces.Count; i++)
+            {
+                Rect tmpBttnRect = new Rect(0f, (bttnHeight + margin) * i, BaseSpeciesBttnSize.x, BaseSpeciesBttnSize.y);
+                Rect descLabelRect = new Rect(0f, tmpBttnRect.yMax - bttnDescHeight, tmpBttnRect.width, bttnDescHeight);
+                StoredRace currentRace = storedPawnRaces[i];
+
+                Widgets.DrawHighlightIfMouseover(tmpBttnRect);
+                Widgets.DrawTextHighlight(descLabelRect);
+
+                Widgets.DefIcon(tmpBttnRect.LeftPartPixels(tmpBttnRect.height), currentRace.ThingDef);
+                Text.Font = GameFont.Tiny;
+                Widgets.Label(descLabelRect.MoveRect(new Vector2(margin + tmpBttnRect.height, 0f)), currentRace?.ThingDef?.LabelCap ?? "<color=red>ThingDef Missing</color>");
+                if (Widgets.ButtonInvisible(tmpBttnRect))
+                {
+                    selectedRace = (i != selectedRace) ? i : -1;
+                }
+            }
+            Widgets.EndGroup();
+            Widgets.EndScrollView();
             ResetFontAndColor();
             if (Widgets.CloseButtonFor(closBttnRect)) Close();
         }

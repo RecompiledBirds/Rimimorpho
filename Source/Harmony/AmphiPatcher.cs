5c681b908711070d46812dc7564187e890c5f4ce
RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2024-03-05 14:00:50 -0500

    Attach Searcher patches

--- Source/Harmony/AmphiPatcher.cs
+++ Source/Harmony/AmphiPatcher.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.AI;
 
 namespace Rimimorpho
 {
@@ -21,6 +22,7 @@ namespace Rimimorpho
             harmony.Patch(AccessTools.Method(typeof(Pawn_FilthTracker), "TryPickupFilth"), postfix: new HarmonyMethod(typeof(TryPickupFilthPatch), nameof(TryPickupFilthPatch.Postfix)));
             harmony.Patch(AccessTools.Method(typeof(JobDriver_CleanFilth), "MakeNewToils"), postfix: new HarmonyMethod(typeof(CleaningPatch), nameof(CleaningPatch.Postfix)));
             harmony.Patch(AccessTools.Method(typeof(SkillUI), nameof(SkillUI.DrawSkillsOf)), prefix: new HarmonyMethod(typeof(SkillPatch), nameof(SkillPatch.Prefix)));
+            harmony.Patch(AccessTools.Method(typeof(AttackTargetsCache), "GetPotentialTargetsFor"), postfix: new HarmonyMethod(typeof(PotentialTargetsPatch),nameof(PotentialTargetsPatch.Postfix)));
             Log.Message("Amphi patches completed!");
         }
     }
---
+++ Source/Harmony/PotentialTargetsPatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace Rimimorpho
+{
+    public static class PotentialTargetsPatch
+    {
+        public static IEnumerable<IAttackTarget> Postfix(IEnumerable<IAttackTarget> values, IAttackTargetSearcher th)
+        {
+            Thing thing = th.Thing;
+            foreach (IAttackTarget target in values) {
+                Thing targThing = target.Thing;
+                AmphiShifter shifter = targThing.TryGetComp<AmphiShifter>();
+                if (shifter == null)
+                {
+                    yield return target;
+                }
+                else
+                {
+                    if (shifter.IsParentDef() || shifter.RaceProperties.Humanlike) { yield return target; } 
+                    else
+                    {
+                        float dist = Vector3.Distance(new Vector3(thing.Position.x,thing.Position.y,thing.Position.z), new Vector3(targThing.Position.x, targThing.Position.y, targThing.Position.z));
+
+                        if (dist < 10f)
+                        {
+                            yield return target;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

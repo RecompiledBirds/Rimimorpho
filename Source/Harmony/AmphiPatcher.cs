5b5044a6ccde82533e8fc8099f71f5856b7845fc
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2024-03-12 20:40:27 -0400

    Merged

--- Source/Harmony/AmphiPatcher.cs
+++ Source/Harmony/AmphiPatcher.cs
@@ -16,12 +16,12 @@ namespace Rimimorpho
     {
         static AmphiPatcher()
         {
-            Log.Message("Running amphi patches");
+            Log.Message("Running Rimimorpho patches");
             Harmony harmony = new Harmony("RecompiledBirds.Rimimorpho");
             harmony.Patch(AccessTools.Method(typeof(Pawn_FilthTracker), "TryPickupFilth"), postfix: new HarmonyMethod(typeof(TryPickupFilthPatch), nameof(TryPickupFilthPatch.Postfix)));
             harmony.Patch(AccessTools.Method(typeof(JobDriver_CleanFilth), "MakeNewToils"), postfix: new HarmonyMethod(typeof(CleaningPatch), nameof(CleaningPatch.Postfix)));
             harmony.Patch(AccessTools.Method(typeof(SkillUI), nameof(SkillUI.DrawSkillsOf)), prefix: new HarmonyMethod(typeof(SkillPatch), nameof(SkillPatch.Prefix)));
-            Log.Message("Amphi patches completed!");
+            Log.Message("Rimimorpho patches completed!");
         }
     }
 }
